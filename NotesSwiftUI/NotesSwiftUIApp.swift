4a83d0f29ce95db10595402bd4dbc4b018062dee
Txxxr <viktor.shmanin@yandex.ru>
2022-09-19 20:43:00 +0300

    Initial Commit

---
+++ NotesSwiftUI/NotesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesSwiftUIApp.swift
+//  NotesSwiftUI
+//
+//  Created by Victor on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
